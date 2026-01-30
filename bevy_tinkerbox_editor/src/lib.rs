a9aadab3222daf9aab13c829e81ee2b0cd764b49
Justin R Johnson <justin.johnson@irongremlin.com>
2026-01-30 12:30:37 -0800

    Move scene persistence to module

--- bevy_tinkerbox_editor/src/lib.rs
+++ bevy_tinkerbox_editor/src/lib.rs
@@ -1,12 +1,10 @@
-use std::{any::TypeId, marker::Send, path::Path};
+use std::any::TypeId;
 
 use bevy::{
-    asset::io::file::FileAssetReader,
     ecs::{
-        component::ComponentId, entity::EntityHashMap, lifecycle::HookContext,
-        reflect::ReflectCommandExt, relationship::Relationship, world::DeferredWorld,
+        component::ComponentId, lifecycle::HookContext, reflect::ReflectCommandExt,
+        relationship::Relationship, world::DeferredWorld,
     },
-    image::{},
     input_focus::tab_navigation::{TabGroup, TabNavigationPlugin},
     platform::collections::HashSet,
     prelude::*,
@@ -17,20 +15,21 @@ use bevy::{
     },
     ui_widgets::{ScrollbarPlugin, observe},
 };
-use bevy_file_dialog::{EntityFileDialogExt, EntityScopedDialogEvent, FileDialogPlugin};
+use bevy_file_dialog::FileDialogPlugin;
 use bevy_ui_text_input::TextInputPlugin;
 
 use crate::{
-    asset_extensions::{
-        AssetServerSaveExtension, SerializationProxies,
-        asset_tracking::{AssetLoadedEvent, ResourceHandles, load_and_watch},
-    },
+    asset_extensions::asset_tracking::ResourceHandles,
     editor_override_traits::*,
     widgets::{
-        add_entity_button::{add_entity_button, make_new_entity_ui},
+        add_entity_button::add_entity_button,
         component_browser::{ComponentBrowserWidgetRoot, ComponentSelection},
         field_input::*,
         general::*,
+        scene_persistence::{
+            ComponentInstantiation, load_scene_dialog, load_scene_with_path, save_scene_dialog,
+            save_scene_with_path,
+        },
         view_only_component::{RideAlongComponent, view_only_component},
     },
 };
@@ -63,8 +62,6 @@ impl Plugin for ComponentEditorPlugin {
         app.init_resource::<AppTypeRegistry>();
         app.add_systems(Startup, editor_initialization);
         app.add_observer(root);
-        app.add_observer(scene_save);
-        app.add_observer(scene_load);
         app.add_observer(component_ui_initializer);
         app.add_observer(on_update_event_dynamic);
 
@@ -81,7 +78,6 @@ impl Plugin for ComponentEditorPlugin {
 pub struct MainEditorCamera;
 #[derive(Component)]
 pub struct SceneViewCamera;
-
 
 #[derive(States, Copy, Clone, Eq, PartialEq, Hash, Debug, Default)]
 pub enum LoadingStatus {
@@ -142,35 +138,8 @@ pub fn spawn_editor(
                             ..default()
                         },
                         BorderColor::from(Srgba::WHITE),
-                        observe(|src: On<Pointer<Click>>, mut commands: Commands| {
-                            commands
-                                .entity(src.event_target())
-                                .with_dialog()
-                                .set_title("Save Scene")
-                                .set_directory("./sample_project_bin/assets/scenes")
-                                .add_filter("scene files", &["ron"])
-                                .pick_file_path();
-                        }),
-                        observe(
-                            |event: On<EntityScopedDialogEvent>, mut commands: Commands| {
-                                info!("Saved! {:?}", event);
-                                use bevy_file_dialog::EntityScopedDialogResult::*;
-                                match event.clone().result {
-                                    Pick(file_pick) => {
-                                        let path = file_pick
-                                            .path
-                                            .clone()
-                                            .to_owned()
-                                            .to_str()
-                                            .unwrap()
-                                            .to_owned();
-
-                                        commands.trigger(SaveScene(path));
-                                    }
-                                    _ => {}
-                                };
-                            }
-                        ),
+                        observe(save_scene_dialog),
+                        observe(save_scene_with_path),
                         children![(Text::new("Save"), TextFont::from_font_size(10.))]
                     ),
                     (
@@ -181,51 +150,8 @@ pub fn spawn_editor(
                             ..default()
                         },
                         BorderColor::from(Srgba::WHITE),
-                        observe(|src: On<Pointer<Click>>, mut commands: Commands| {
-                            commands
-                                .entity(src.event_target())
-                                .with_dialog()
-                                .set_title("Load Scene")
-                                .set_directory("./sample_project_bin/assets/scenes")
-                                .add_filter("scene files", &["ron"])
-                                .pick_file_path();
-                        }),
-                        observe(
-                            |event: On<EntityScopedDialogEvent>, mut commands: Commands| {
-                                info!("Loaded! {:?}", event);
-                                use bevy_file_dialog::EntityScopedDialogResult::*;
-                                match event.clone().result {
-                                    Pick(file_pick) => {
-                                        let full_path = file_pick.path.clone().to_owned();
-                                        //TODO - figure out how to do this without hard-coding this string.
-                                        let root = FileAssetReader::get_base_path()
-                                            .parent()
-                                            .unwrap()
-                                            .join("sample_project_bin")
-                                            .join("assets");
-
-                                        let fp_ = full_path.clone();
-                                        let fp = fp_.to_string_lossy();
-                                        let r_ = root.clone();
-                                        let r = r_.to_string_lossy();
-
-                                        let path = full_path
-                                            .strip_prefix(root)
-                                            .map_err(move |e| {
-                                                info!("Could not strip {r} path from {fp}",);
-                                                info!("{:?}", e.to_string());
-                                                e
-                                            })
-                                            .unwrap()
-                                            .to_str()
-                                            .unwrap()
-                                            .to_owned();
-                                        commands.trigger(LoadScene(path));
-                                    }
-                                    _ => {}
-                                };
-                            }
-                        ),
+                        observe(load_scene_dialog),
+                        observe(load_scene_with_path),
                         children![(Text::new("Load"), TextFont::from_font_size(10.))]
                     )
                 ],
@@ -269,121 +195,6 @@ pub fn spawn_editor(
             )
         ],
     ));
-}
-
-#[derive(Event)]
-struct SaveScene(pub String);
-#[derive(Event)]
-struct LoadScene(pub String);
-
-fn scene_save(src: On<SaveScene>, world: DeferredWorld) {
-    let mut ui_roots = world
-        .try_query::<&EntityUiRoot>()
-        .expect("Failed to instantiate query");
-    let type_registry = world.resource::<AppTypeRegistry>();
-    let proxy_registry = world.resource::<SerializationProxies>();
-
-    let mut tiny_world = World::new();
-    tiny_world.insert_resource(type_registry.clone());
-    let mut mapper = EntityHashMap::<Entity>::new();
-    let mut aggregate: HashSet<TypeId> = HashSet::new();
-
-    ui_roots.iter(&world).for_each(|ui_root| {
-        let desired_serialization_components: HashSet<TypeId> = ui_root
-            .desired_component_set
-            .iter()
-            .map(|t_id| {
-                let serialization_proxy = proxy_registry.get_proxy(&t_id);
-                *(serialization_proxy.unwrap_or(t_id))
-            })
-            .collect();
-        info!(
-            "Entity: {:?} with {:?} components",
-            ui_root.component_holder,
-            desired_serialization_components.len()
-        );
-        for x in desired_serialization_components.iter() {
-            aggregate.insert(x.clone());
-        }
-
-        let smol_scene = DynamicSceneBuilder::from_world(&*world)
-            .with_component_filter(SceneFilter::Allowlist(desired_serialization_components))
-            .extract_entity(ui_root.component_holder)
-            .build();
-        let r =
-            smol_scene.write_to_world_with(&mut tiny_world, &mut mapper, &type_registry.clone());
-        if r.is_err() {
-            info!("{:?}", r);
-        }
-    });
-    let big_scene = DynamicSceneBuilder::from_world(&tiny_world)
-        .with_component_filter(SceneFilter::Allowlist(aggregate))
-        .deny_all_resources()
-        .extract_entities(mapper.iter().map(|(_k, v)| *v))
-        .build();
-
-    for ent in big_scene.entities.iter() {
-        info!(
-            "Saving: Entity: {:?} with {:?} components",
-            ent.entity,
-            ent.components.len()
-        );
-    }
-    AssetServer::save_dynamic_scene(Path::new(&src.0), type_registry, big_scene);
-}
-
-fn scene_bounce(
-    src: On<AssetLoadedEvent<DynamicScene>>,
-    scenes: Res<Assets<DynamicScene>>,
-    proxies: Res<SerializationProxies>,
-    find_anchor: Query<Entity, With<SelectedEntityUiRoot>>,
-    mut commands: Commands,
-) {
-    let Ok(anchor) = find_anchor.single() else {
-        return;
-    };
-    let dyn_scene = scenes
-        .get(src.handle.id())
-        .expect("Dynamic scene asset failed to exist");
-    for dyn_entity in dyn_scene.entities.iter() {
-        let world_target = commands.spawn_empty().id();
-        let new_entity_ui = commands.spawn_empty().id();
-        commands.entity(anchor).add_child(new_entity_ui);
-        commands
-            .entity(new_entity_ui)
-            .insert(make_new_entity_ui(world_target));
-
-        for box_component in dyn_entity.components.iter() {
-            let og_tinfo = box_component
-                .get_represented_type_info()
-                .expect("Valid Type Info");
-            let og_tid = og_tinfo.type_id().clone();
-            let type_id = proxies.get_target(og_tinfo.type_path()).unwrap_or(&og_tid);
-
-            commands.entity(world_target).insert_reflect(
-                box_component
-                    .reflect_clone()
-                    .expect("Failed to clone component"),
-            );
-            commands.trigger(ComponentInstantiation {
-                entity: new_entity_ui,
-                type_id: type_id.clone(),
-            });
-        }
-    }
-    commands.entity(src.event_target()).despawn();
-}
-
-fn scene_load(src: On<LoadScene>, asset_server: Res<AssetServer>, mut commands: Commands) {
-    let asset_bucket = commands.spawn_empty().id();
-    commands.entity(asset_bucket).observe(scene_bounce);
-    load_and_watch::<DynamicScene>(asset_bucket, &mut commands, &src.0, &*asset_server);
-}
-
-#[derive(EntityEvent, Clone)]
-struct ComponentInstantiation {
-    entity: Entity,
-    type_id: TypeId,
 }
 
 // TODO -
---
+++ bevy_tinkerbox_editor/src/widgets/scene_persistence.rs
@@ -0,0 +1,209 @@
+use std::{any::TypeId, path::Path};
+
+use ::bevy::prelude::*;
+use bevy::{
+    asset::io::file::FileAssetReader,
+    ecs::{entity::EntityHashMap, reflect::ReflectCommandExt, world::DeferredWorld},
+    platform::collections::HashSet,
+};
+use bevy_file_dialog::{EntityFileDialogExt, EntityScopedDialogEvent};
+
+use crate::{
+    EntityUiRoot, SelectedEntityUiRoot,
+    asset_extensions::{
+        AssetServerSaveExtension, SerializationProxies,
+        asset_tracking::{AssetLoadedEvent, load_and_watch},
+    },
+    widgets::add_entity_button::make_new_entity_ui,
+};
+
+pub(super) fn plugin(app: &mut App) {
+    app.add_observer(scene_save);
+    app.add_observer(scene_load);
+}
+
+#[derive(Event)]
+pub struct SaveScene(pub String);
+#[derive(Event)]
+pub struct LoadScene(pub String);
+
+fn scene_save(src: On<SaveScene>, world: DeferredWorld) {
+    let mut ui_roots = world
+        .try_query::<&EntityUiRoot>()
+        .expect("Failed to instantiate query");
+    let type_registry = world.resource::<AppTypeRegistry>();
+    let proxy_registry = world.resource::<SerializationProxies>();
+
+    let mut tiny_world = World::new();
+    tiny_world.insert_resource(type_registry.clone());
+    let mut mapper = EntityHashMap::<Entity>::new();
+    let mut aggregate: HashSet<TypeId> = HashSet::new();
+
+    ui_roots.iter(&world).for_each(|ui_root| {
+        let desired_serialization_components: HashSet<TypeId> = ui_root
+            .desired_component_set
+            .iter()
+            .map(|t_id| {
+                let serialization_proxy = proxy_registry.get_proxy(&t_id);
+                *(serialization_proxy.unwrap_or(t_id))
+            })
+            .collect();
+        info!(
+            "Entity: {:?} with {:?} components",
+            ui_root.component_holder,
+            desired_serialization_components.len()
+        );
+        for x in desired_serialization_components.iter() {
+            aggregate.insert(x.clone());
+        }
+
+        let smol_scene = DynamicSceneBuilder::from_world(&*world)
+            .with_component_filter(SceneFilter::Allowlist(desired_serialization_components))
+            .extract_entity(ui_root.component_holder)
+            .build();
+        let r =
+            smol_scene.write_to_world_with(&mut tiny_world, &mut mapper, &type_registry.clone());
+        if r.is_err() {
+            info!("{:?}", r);
+        }
+    });
+    let big_scene = DynamicSceneBuilder::from_world(&tiny_world)
+        .with_component_filter(SceneFilter::Allowlist(aggregate))
+        .deny_all_resources()
+        .extract_entities(mapper.iter().map(|(_k, v)| *v))
+        .build();
+
+    for ent in big_scene.entities.iter() {
+        info!(
+            "Saving: Entity: {:?} with {:?} components",
+            ent.entity,
+            ent.components.len()
+        );
+    }
+    AssetServer::save_dynamic_scene(Path::new(&src.0), type_registry, big_scene);
+}
+
+fn scene_bounce(
+    src: On<AssetLoadedEvent<DynamicScene>>,
+    scenes: Res<Assets<DynamicScene>>,
+    proxies: Res<SerializationProxies>,
+    find_anchor: Query<Entity, With<SelectedEntityUiRoot>>,
+    mut commands: Commands,
+) {
+    let Ok(anchor) = find_anchor.single() else {
+        return;
+    };
+    let dyn_scene = scenes
+        .get(src.handle.id())
+        .expect("Dynamic scene asset failed to exist");
+    for dyn_entity in dyn_scene.entities.iter() {
+        let world_target = commands.spawn_empty().id();
+        let new_entity_ui = commands.spawn_empty().id();
+        commands.entity(anchor).add_child(new_entity_ui);
+        commands
+            .entity(new_entity_ui)
+            .insert(make_new_entity_ui(world_target));
+
+        for box_component in dyn_entity.components.iter() {
+            let og_tinfo = box_component
+                .get_represented_type_info()
+                .expect("Valid Type Info");
+            let og_tid = og_tinfo.type_id().clone();
+            let type_id = proxies.get_target(og_tinfo.type_path()).unwrap_or(&og_tid);
+
+            commands.entity(world_target).insert_reflect(
+                box_component
+                    .reflect_clone()
+                    .expect("Failed to clone component"),
+            );
+            commands.trigger(ComponentInstantiation {
+                entity: new_entity_ui,
+                type_id: type_id.clone(),
+            });
+        }
+    }
+    commands.entity(src.event_target()).despawn();
+}
+
+fn scene_load(src: On<LoadScene>, asset_server: Res<AssetServer>, mut commands: Commands) {
+    let asset_bucket = commands.spawn_empty().id();
+    commands.entity(asset_bucket).observe(scene_bounce);
+    load_and_watch::<DynamicScene>(asset_bucket, &mut commands, &src.0, &*asset_server);
+}
+
+#[derive(EntityEvent, Clone)]
+pub struct ComponentInstantiation {
+    pub entity: Entity,
+    pub type_id: TypeId,
+}
+pub fn save_scene_dialog(src: On<Pointer<Click>>, mut commands: Commands) {
+    commands
+        .entity(src.event_target())
+        .with_dialog()
+        .set_title("Save Scene")
+        .set_directory("./sample_project_bin/assets/scenes")
+        .add_filter("scene files", &["ron"])
+        .pick_file_path();
+}
+pub fn save_scene_with_path(event: On<EntityScopedDialogEvent>, mut commands: Commands) {
+    info!("Saved! {:?}", event);
+    use bevy_file_dialog::EntityScopedDialogResult::*;
+    match event.clone().result {
+        Pick(file_pick) => {
+            let path = file_pick
+                .path
+                .clone()
+                .to_owned()
+                .to_str()
+                .unwrap()
+                .to_owned();
+
+            commands.trigger(SaveScene(path));
+        }
+        _ => {}
+    };
+}
+pub fn load_scene_with_path(event: On<EntityScopedDialogEvent>, mut commands: Commands) {
+    info!("Loaded! {:?}", event);
+    use bevy_file_dialog::EntityScopedDialogResult::*;
+    match event.clone().result {
+        Pick(file_pick) => {
+            let full_path = file_pick.path.clone().to_owned();
+            //TODO - figure out how to do this without hard-coding this string.
+            let root = FileAssetReader::get_base_path()
+                .parent()
+                .unwrap()
+                .join("sample_project_bin")
+                .join("assets");
+
+            let fp_ = full_path.clone();
+            let fp = fp_.to_string_lossy();
+            let r_ = root.clone();
+            let r = r_.to_string_lossy();
+
+            let path = full_path
+                .strip_prefix(root)
+                .map_err(move |e| {
+                    info!("Could not strip {r} path from {fp}",);
+                    info!("{:?}", e.to_string());
+                    e
+                })
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_owned();
+            commands.trigger(LoadScene(path));
+        }
+        _ => {}
+    };
+}
+
+pub fn load_scene_dialog(src: On<Pointer<Click>>, mut commands: Commands) {
+    commands
+        .entity(src.event_target())
+        .with_dialog()
+        .set_title("Load Scene")
+        .set_directory("./sample_project_bin/assets/scenes")
+        .add_filter("scene files", &["ron"])
+        .pick_file_path();
+}
